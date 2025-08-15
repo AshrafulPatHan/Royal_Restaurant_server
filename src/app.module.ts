2726dfc25023c25a2e4d186fd5d92cbc24b0b7ad
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-08-15 20:31:39 +0600

    add now project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AdminModule } from './admin/admin.module';
-import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AdminModule, UserModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
