5a776ac51e7d8ad8b3d2261e3e4474a9bf78979b
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-08-15 20:38:50 +0600

    server is running 1.0

--- doc/archive/Toturual.md
+++ doc/archive/Toturual.md
@@ -1,5 +1,9 @@
 # how to code
 
+## start the server
+npm run start
+npm run start:dev
+
 ## for create new roust
 nest generate module user
 nest generate controller user
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,5 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
-  }
+    return 'server is running 1.0!';  }
 }
