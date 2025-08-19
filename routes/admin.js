1a11fa3baafb37ed64b62cbba89e15fa357bf53a
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-08-19 10:48:57 +0600

    add all get requist

--- routes/admin.js
+++ routes/admin.js
@@ -39,7 +39,135 @@ routes.post('/auth', async (req, res) => {
         console.error("error is coming to login admin",error);
         res.status(500).send("❌ Login Error: " + error.message);
     }
-})
+});
+
+// add menu
+routes.post('/add-menu', async (req,res)=>{
+    try{
+        const data = req.body;
+        const result = await Menu.insertOne(data);
+        res.status(200).send(result);
+    }catch(error){
+        console.error("error is coming to login admin",error);
+    }
+});
+
+// add Blog
+routes.post('/add-blog', async (req,res)=>{
+    try{
+        const data = req.body;
+        const result = await Blog.insertOne(data);
+        res.status(200).send(result);
+    }catch(error){
+        console.error("error is coming to login admin",error);
+    }
+});
+
+// add Table
+routes.post('/add-table', async (req,res)=>{
+    try{
+        const data = req.body;
+        const result = await AllTable.insertOne(data);
+        res.status(200).send(result);
+    }catch(error){
+        console.error("error is coming to login admin",error);
+    }
+});
+
+// ---- get request
+
+// all menu
+routes.get('/all-menu', async (req, res) => {
+try {
+    const cursor = Menu.find();
+    const result = await cursor.toArray();
+    res.send(result);
+} catch (error) {
+    console.error('Error retrieving data:', error);
+    res.status(500).send({ message: 'Internal Server Error' });
+}
+});
+
+// all review
+routes.get('/all-review', async (req, res) => {
+try {
+    const cursor = Review.find();
+    const result = await cursor.toArray();
+    res.send(result);
+} catch (error) {
+    console.error('Error retrieving data:', error);
+    res.status(500).send({ message: 'Internal Server Error' });
+}
+});
+
+// all order
+routes.get('/all-order', async (req, res) => {
+try {
+    const cursor = Order.find();
+    const result = await cursor.toArray();
+    res.send(result);
+} catch (error) {
+    console.error('Error retrieving data:', error);
+    res.status(500).send({ message: 'Internal Server Error' });
+}
+});
+
+// all reservation
+routes.get('/all-reservation', async (req, res) => {
+try {
+    const cursor = Reservation.find();
+    const result = await cursor.toArray();
+    res.send(result);
+} catch (error) {
+    console.error('Error retrieving data:', error);
+    res.status(500).send({ message: 'Internal Server Error' });
+}
+});
+
+// all mail
+routes.get('/all-mail', async (req, res) => {
+try {
+    const cursor = Mail.find();
+    const result = await cursor.toArray();
+    res.send(result);
+} catch (error) {
+    console.error('Error retrieving data:', error);
+    res.status(500).send({ message: 'Internal Server Error' });
+}
+});
+
+// all blog
+routes.get('/all-blog', async (req, res) => {
+try {
+    const cursor = Blog.find();
+    const result = await cursor.toArray();
+    res.send(result);
+} catch (error) {
+    console.error('Error retrieving data:', error);
+    res.status(500).send({ message: 'Internal Server Error' });
+}
+});
+
+
+// ---- delete request (jwt need)
+
+// delete review
+
+// delete menu
+
+// delete order
+
+// delete blog
+
+
+// ---- update request (jwt need)
+
+// update menu
+
+// update review
+
+// update blog
+
 
 return routes
 }
