ed84b5871fdbd1ef491c7f87fcfce28b6b0ac998
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-08-20 08:58:17 +0600

    add the get public requist on server

--- routes/public.js
+++ routes/public.js
@@ -8,6 +8,56 @@ routes.get('/user', (req,res) =>{
     res.send('user routes')
 })
 
+// all review
+routes.get('/all-review', async (req, res) => {
+try {
+    const cursor = Review.find();
+    const result = await cursor.toArray();
+    res.send(result);
+} catch (error) {
+    console.error('Error retrieving data:', error);
+    res.status(500).send({ message: 'Internal Server Error' });
+}
+});
+
+// all menu
+routes.get('/all-menu', async (req, res) => {
+try {
+    const cursor = Menu.find();
+    const result = await cursor.toArray();
+    res.send(result);
+} catch (error) {
+    console.error('Error retrieving data:', error);
+    res.status(500).send({ message: 'Internal Server Error' });
+}
+});
+
+// letest menu
+routes.get('/letest-menu', async (req, res) => {
+try {
+    const cursor = Review.find();
+    const result = await cursor.toArray();
+    res.send(result);
+} catch (error) {
+    console.error('Error retrieving data:', error);
+    res.status(500).send({ message: 'Internal Server Error' });
+}
+});
+
+
+// all Blog
+routes.get('/all-menu', async (req, res) => {
+try {
+    const cursor = Review.find();
+    const result = await cursor.toArray();
+    res.send(result);
+} catch (error) {
+    console.error('Error retrieving data:', error);
+    res.status(500).send({ message: 'Internal Server Error' });
+}
+});
+
+
 
 return routes;
 }
